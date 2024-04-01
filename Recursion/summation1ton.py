2ce1cb9297cfc80356b977df2886be6cb278e404
Prathamesh <prathameshmane855@gmail.com>
2024-04-01 21:43:07 +0530

    Summation Function wise completed

---
+++ Recursion/summation1ton.py
@@ -0,0 +1,30 @@
+# Parameter way
+N=3
+sum1=0
+def sumParameter(i,sum1):
+    if i < 1:
+        return sum1
+    sumParameter(i-1,sum1+i)
+
+
+sumParameter(N,sum1)
+
+
+
+
+
+
+# Functional way
+
+N=3
+# Backtracking
+def sumN(N):
+    if N==1:
+        return N
+    sum1=N+ sumN(N-1)
+    return sum1
+    
+output=sumN(N)
+print(output)
+    
+    
